fccbe052c08fc288b52f2076b9f08a72aaf09db2
Vijayalakshmi Sankati <vijayareddy1998@gmail.com>
2023-04-18 15:20:21 -0500

    performing update deletes on forms

--- controllers/vehicals.js
+++ controllers/vehicals.js
@@ -1,80 +1,80 @@
 var vehicals = require('../models/vehicals');
 // List of all Costumes
-exports.vehicals_list = function(req, res) {
-res.send('NOT IMPLEMENTED: vehicals list');
+exports.vehicals_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicals list');
 };
 // for a specific Costume.
-exports.vehicals_detail = async function(req, res) {
+exports.vehicals_detail = async function (req, res) {
     console.log("detail" + req.params.id)
     try {
-    result = await vehicals.findById( req.params.id)
-    res.send(result)
+        result = await vehicals.findById(req.params.id)
+        res.send(result)
     } catch (error) {
-    res.status(500)
-    res.send(`{"error": document for id ${req.params.id} not found`);
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
     }
-   };
+};
 // for a specific Costume.
 // exports.vehicals_detail = function(req, res) {
 // res.send('NOT IMPLEMENTED: vehicals detail: ' + req.params.id);
 // };
 // Handle Costume create on POST.
-exports.vehicals_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: vehicals create POST');
+exports.vehicals_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicals create POST');
 };
 // Handle Costume delete form on DELETE.
-exports.vehicals_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: vehicals delete DELETE ' + req.params.id);
+exports.vehicals_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicals delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
 // exports.vehicals_update_put = function(req, res) {
 // res.send('NOT IMPLEMENTED: vehicals update PUT' + req.params.id);
 // };
 // Handle Costume update form on PUT.
-exports.vehicals_update_put = async function(req, res) {
- console.log(`update on id ${req.params.id} with body
+exports.vehicals_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
- try {
- let toUpdate = await vehicals.findById( req.params.id)
- // Do updates of properties
- if(req.body.type)
- toUpdate.type = req.body.type;
- if(req.body.milage) toUpdate.milage = req.body.milage;
- if(req.body.price) toUpdate.price = req.body.price;
- let result = await toUpdate.save();
- console.log("Sucess " + result)
- res.send(result)
- } catch (err) {
- res.status(500)
- res.send(`{"error": ${err}: Update for id ${req.params.id}
+    try {
+        let toUpdate = await vehicals.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.type)
+            toUpdate.type = req.body.type;
+        if (req.body.milage) toUpdate.milage = req.body.milage;
+        if (req.body.price) toUpdate.price = req.body.price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
 failed`);
- }
+    }
 };
 // List of all Costumes
-exports.vehicals_list = async function(req, res) {
-    try{
-    theVehicals = await vehicals.find();
-    res.send(theVehicals);
+exports.vehicals_list = async function (req, res) {
+    try {
+        theVehicals = await vehicals.find();
+        res.send(theVehicals);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    // VIEWS
+};
+// VIEWS
 // Handle a show all view
-exports.vehicals_view_all_Page = async function(req, res) {
-    try{
-    theVehicals = await vehicals.find();
-    res.render('vehicals', { title: 'Vehical Search Results', results: theVehicals });
+exports.vehicals_view_all_Page = async function (req, res) {
+    try {
+        theVehicals = await vehicals.find();
+        res.render('vehicals', { title: 'Vehical Search Results', results: theVehicals });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    // Handle Costume create on POST.
-exports.vehicals_create_post = async function(req, res) {
+};
+// Handle Costume create on POST.
+exports.vehicals_create_post = async function (req, res) {
     console.log(req.body)
     let document = new vehicals();
     // We are looking for a body, since POST does not have query parameters.
@@ -84,12 +84,81 @@ exports.vehicals_create_post = async function(req, res) {
     document.type = req.body.type;
     document.milage = req.body.milage;
     document.price = req.body.price;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+// Handle Costume delete on DELETE.
+exports.vehicals_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await vehicals.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
+//Code added for SS6
+// Handle a show one view with id specified by query
+exports.vehicals_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await vehicals.findById(req.query.id)
+        res.render('vehicalsdetail',
+            { title: 'vehicals Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+//SS7
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.vehicals_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('vehicalscreate', { title: 'vehicals Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+//SS8
+// Handle building the view for updating a costume.
+// query provides the id
+exports.vehicals_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await vehicals.findById(req.query.id)
+        res.render('vehicalsupdate', { title: 'vehicals Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+//SS9
+// Handle a delete one view with id from query
+exports.vehicals_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
     try{
-    let result = await document.save();
-    res.send(result);
+    result = await vehicals.findById(req.query.id)
+    res.render('vehicalsdelete', { title: 'vehicals Delete', toShow:
+    result });
     }
     catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
     };
--- routes/vehicals.js
+++ routes/vehicals.js
@@ -5,3 +5,15 @@ const vehicals_controlers= require('../controllers/vehicals');
 /* GET costumes */
 router.get('/', vehicals_controlers.vehicals_view_all_Page );
 module.exports = router;
+//SS6
+/* GET detail costume page */
+router.get('/detail', vehicals_controlers.vehicals_view_one_Page);
+//SS7
+/* GET create costume page */
+router.get('/create', vehicals_controlers.vehicals_create_Page);
+//SS8
+/* GET create update page */
+router.get('/update', vehicals_controlers.vehicals_update_Page);
+//SS9
+/* GET delete costume page */
+router.get('/delete', vehicals_controlers.vehicals_delete_Page);
